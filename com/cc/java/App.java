f4b52ffe572c6eefc395b7328234d359953fbc0e
Yasmeen <yasmeenmas@gmx.de>
2023-07-28 09:21:06 +0200

    Antons Lösung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,5 +1,9 @@
 package com.cc.java;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
     
     public static void main(String[] args) {
@@ -29,5 +33,33 @@ public class App {
     private static void output(String outputStr) {
         System.out.println(outputStr);
     } 
+
+    
+// Antons Lösung
+
+//  List<Konto> konten = new ArrayList<Konto>(Arrays.asList(new Konto(),
+//                                                                 new Konto(), 
+//                                                                 new Konto()));
+
+//         output(konten);
+
+//         konten.get(0).setKontostand(konten.get(0).getKontostand() * 2);
+//         konten.get(1).setKontostand(konten.get(1).getKontostand() * 3);
+//         konten.get(2).setKontostand(konten.get(2).getKontostand() * 10);
+
+//         output("---------------------------------");
+
+//         output(konten);
+//     }
+
+//     private static void output(String outputStr) {
+//         System.out.println(outputStr);
+//     }
+
+//     private static void output(List<Konto> konten) {
+//         for (Konto konto : konten) {
+//             output(String.valueOf(konto.getKontostand()));
+//         }
+
 
 }
